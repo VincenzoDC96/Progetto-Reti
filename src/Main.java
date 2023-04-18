aec23d2198dc329aea7f66d56e4266612d2da26d
Henzo <vincenzo.carfora96@gmail.com>
2023-04-18 11:58:24 +0200

    prima riga aggiunta

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("oooo");
         System.out.println("ciao bello");
         System.out.println("Guglugu");
+        System.out.println("Guglugu");
     }
 }
