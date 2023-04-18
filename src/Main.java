ba4b7346046519b6f29c74fcf58f654d6ee34bb3
Giovanni <gianni9825@gmail.com>
2023-04-18 11:46:54 +0200

    seconda riga

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Facciamo qualche cambio");
         System.out.println("oooo");
+        System.out.println("ciao bello");
     }
 }
