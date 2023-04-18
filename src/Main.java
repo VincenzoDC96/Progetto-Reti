2881096ffc62d7feb1f6ba6d760b454a61ca7c6d
Henzo <vincenzo.carfora96@gmail.com>
2023-04-18 12:01:20 +0200

    prima riga aggiunta

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("popo");
         System.out.println("Facciamo qualche cambio");
         System.out.println("oooo");
         System.out.println("ciao bello");
